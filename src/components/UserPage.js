c73df7e5fef36ef52f5da42f6b378a923cc3da0a
Yahyagok <ygok17@hotmail.com>
2021-03-19 23:15:35 -0400

    unique id's for favorite page

--- src/components/UserPage.js
+++ src/components/UserPage.js
@@ -1,15 +1,19 @@
 import React from 'react'
 import {connect} from 'react-redux'
-
 const UserPage =(props) => {
-   let favoriteActors = props.favorites.filter(fav => fav.attributes.user.id === props.userId )
-   let uniqueActors = favoriteActors.filter((v, i, a) => a.indexOf(v) === i); 
+//    let favoriteActors = props.favorites.filter(fav => fav.attributes.user.id === props.userId)
+// let uniqueActors = props.favorites.filter((item, i, ar) => ar.indexOf(item) === i);
+// let uniqueActors = [...new Set(props.favorites.attributes.user.id )]
 //  console.log(props.favorites)
+const uniqueActors = [...new Set(props.favorites.map(fav => fav.attributes.actor && fav.attributes.actor.name))]
+
+
+
+console.log(uniqueActors)
     return (
         <div>
-            {uniqueActors.map(fav => 
-                <div> {fav.attributes.actor.name}</div>
-
+            { uniqueActors.map(fav => 
+                <div> { fav.attributes.actor  ? fav.attributes.actor.name : null }</div>
                     )}
         </div>
     )
