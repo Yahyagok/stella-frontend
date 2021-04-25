33595f63238e7d0dd1a3f0d3dcc75bcd02e7f13a
Yahyagok <ygok17@hotmail.com>
2021-04-25 11:25:15 -0400

    deleted some comments

--- src/actions/loginUser.js
+++ src/actions/loginUser.js
@@ -1,7 +1,5 @@
 import axios from 'axios'
-
 export function loginUser () {
- 
     return (dispatch) => {
         fetch('http://localhost:3000/logged_in', {withCredentials: true})
         .then(response => response.json())
@@ -15,25 +13,3 @@ export function loginUser () {
           .catch(error => console.log('api errors:', error))
         }   
     }
-
-
-// handleLogin = (data) => { 
-//     setState({
-//       isLoggedIn: true,
-//       user: data.user
-//     },
-//      () => 
-//     {localStorage.user_id = data.user.id}
-//     )
-//   }
-// handleLogout = () => {
-//     setState({
-//     isLoggedIn: false,
-//     user: {}
-//     }, () => {
-//       localStorage.removeItem("user_id")
-//       props.history.push('/login')
-//     })
-//   } 
-
-//user => dispatch({ type: 'LOGIN_USER', payload: user })
