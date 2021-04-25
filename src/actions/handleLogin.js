af90e00dbe68227e70fb131ddb4fbead774d6407
Yahyagok <ygok17@hotmail.com>
2021-04-25 11:24:31 -0400

    deleted some comments

--- src/actions/handleLogin.js
+++ src/actions/handleLogin.js
@@ -1,7 +1,6 @@
 export function handleLogin(){
     axios.post('http://localhost:3000/login', {user}, {withCredentials: true})
       .then(response => {
-        // console.log(response)
         if (response.data.logged_in) {
           props.handleLogin(response.data)
           props.redirect()
@@ -14,9 +13,3 @@ export function handleLogin(){
       .catch(error => console.log('api errors:', error))
  }
 
- // how would I add props to action , or this will stay on Login js 
-
-
-//  Login(handlLoginFunction, ...) {
-//      handleLoginFunction(response.data)
-//  }
